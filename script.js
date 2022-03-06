295f3c18be3b2ccb13c95fe5af245f37bccc616e
harunwafula <harunsimiyuwafula@gmail.com>
2022-03-07 00:25:42 +0200

    finished basic styling

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var slideIndex = 0; //starting index
 var jumbotronImages = document.getElementsByClassName('imageSlides'); // get all images
 var ctaSlides = document.getElementsByClassName('CTAslides');
+var curImageSlide = jumbotronImages[0];
+var curCTASlide = ctaSlides[0];
 console.log(jumbotronImages)
 showSlides();
 
@@ -10,14 +12,24 @@ function plusSlides(n, no) {
 
 function showSlides() {
   var i;
+  // set the display for all of them to none
+  // for (i = 0; i < jumbotronImages.length; i++) {
+  //   jumbotronImages[i].style.display = "none";
+  //   jumbotronImages[i].classList.remove()
+  //   ctaSlides[i].style.display = "none"
+  // }
+  curImageSlide.classList.remove("active");
+  curCTASlide.classList.remove("active");
 
-  for (i = 0; i < jumbotronImages.length; i++) {
-    jumbotronImages[i].style.display = "none";
-    ctaSlides[i].style.display = "none"
-  }
   slideIndex++;
   if (slideIndex > jumbotronImages.length) {slideIndex = 1}
-  jumbotronImages[slideIndex-1].style.display = "block";
-  ctaSlides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000);
+  
+ 
+  // curImageSlide[slideIndex-1].className += " active"
+  curImageSlide = jumbotronImages[slideIndex-1];
+  curCTASlide = ctaSlides[slideIndex-1];
+  curImageSlide.className += " active"
+  curCTASlide.className += " active"
+  // ctaSlides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); //Change slide every 5 seconds
 }
