e98a035e78f7bf3cf5009d4a54ee9bbf6300ecac
Harun Wafula <harunsimiyuwafula@gmail.com>
2022-03-09 22:05:35 +0200

    added js scroll animations

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ var jumbotronImages = document.getElementsByClassName('imageSlides'); // get all
 var ctaSlides = document.getElementsByClassName('CTAslides');
 var curImageSlide = jumbotronImages[0];
 var curCTASlide = ctaSlides[0];
-console.log(jumbotronImages)
 showSlides();
+
+
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
@@ -18,6 +19,12 @@ function showSlides() {
   //   jumbotronImages[i].classList.remove()
   //   ctaSlides[i].style.display = "none"
   // }
+  swipeSlideToLeft();
+  // ctaSlides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); //Change slide every 5 seconds
+}
+
+function swipeSlideToLeft() {
   curImageSlide.classList.remove("active");
   curCTASlide.classList.remove("active");
 
@@ -30,6 +37,89 @@ function showSlides() {
   curCTASlide = ctaSlides[slideIndex-1];
   curImageSlide.className += " active"
   curCTASlide.className += " active"
-  // ctaSlides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); //Change slide every 5 seconds
 }
+
+function swipeSlideToRight() {
+  curImageSlide.classList.remove("active");
+  curCTASlide.classList.remove("active");
+
+  slideIndex--;
+  if (slideIndex < 1) {slideIndex = jumbotronImages.length}
+  
+ 
+  // curImageSlide[slideIndex-1].className += " active"
+  curImageSlide = jumbotronImages[slideIndex-1];
+  curCTASlide = ctaSlides[slideIndex-1];
+  curImageSlide.className += " active"
+  curCTASlide.className += " active"
+}
+
+let touchstartX = 0
+let touchendX = 0
+
+
+// Swipe to change image on jumbotron
+const slider = document.getElementById('jumbotron');
+
+function handleGesture() {
+  if (touchendX < touchstartX) {
+    swipeSlideToLeft();
+  }
+  if (touchendX > touchstartX){
+    swipeSlideToRight();  
+  }
+}
+
+slider.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+})
+
+slider.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX
+  handleGesture()
+})
+
+
+// Scroll animations
+const scrollElements = document.querySelectorAll(".js-scroll");
+console.log(scrollElements);
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      console.log("true")
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+
+  handleScrollAnimation();
+});
